f786652f98894e95a1889ac36e5974e39661952c
Shinde Vinayak Rao Patil <shindevinayakraopatil@gmail.com>
2025-02-28 09:26:40 +0530

    Added new directory week05-python

---
+++ week05-python/Solution.py
@@ -0,0 +1,23 @@
+class person:
+    def __init__(self,name , number=None) -> None:
+
+        self.name = name
+        self.number = None
+    
+    def get_name(self):
+        pass
+    def get_number(self):
+        pass
+    def set_number(self,new_number):
+        pass
+
+    def __str__(self) -> str:
+        pass
+    def __eq__(self, __value: object) -> bool:
+        pass
+
+class ContactList:
+    def __init__(self) -> None:
+        self. preson = []
+        self.file_name=
+
